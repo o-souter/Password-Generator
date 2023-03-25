9f84c47b0f784a8dd1a140bfeae37a2a2545aa2e
Oliver Souter <73543366+o-souter@users.noreply.github.com>
2023-03-25 13:30:29 +0000

    fixed bugs

--- PasswordGenerator/scripts.js
+++ PasswordGenerator/scripts.js
@@ -1,9 +1,10 @@
 var password = document.getElementById("password");
 
 function createPass() {
-    var length = 10;
-    var options = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()QWERTYUIOPASDFGHJKLZXCVBNMoliveriscool";
-    var words = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"];
+    var length = 16;
+    var options = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()QWERTYUIOPASDFGHJKLZXCVBNM";
+    //Optional word option (disabled by default)
+    //var words = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu"];
     var pass = "";
     //Insert random characters from the list to the password
     for (var i = 0; i <= length; i++) {
@@ -11,9 +12,11 @@ function createPass() {
         pass += options.substring(mathRandom, mathRandom+1)
     }
     //Insert a random word from the list to the password
-    var wordChosen = words[Math.floor(Math.random() * words.length)]
-    var wordplace = mathRandom = Math.floor(Math.random() * pass.length)
-    pass = pass.slice(0, wordplace) + wordChosen + pass.slice(wordplace)
+    //var wordChosen = words[Math.floor(Math.random() * words.length)]
+    //var wordplace = mathRandom = Math.floor(Math.random() * pass.length)
+    //pass = pass.slice(0, wordplace) + wordChosen + pass.slice(wordplace)
+
+
     document.getElementById("pass").value = pass;
 }
 
